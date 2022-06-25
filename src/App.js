fa086bbdb6cb6bdf7f5a3edcc29938472ffb7a96
Saransh <saru10081999@gmail.com>
2022-06-25 13:04:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Changes to Test Auto Deployment
         </p>
         <a
           className="App-link"
